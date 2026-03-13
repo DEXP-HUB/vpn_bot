0168213d485cb40d9f3f5b05b056d3bd92a51737
Ali <ali_guseynov_02@mail.ru>
2026-03-13 16:04:36 +0300

    установлен python dotenv и исправлена ошибка импорта в main.py так же реализована ручка test для проверки работы бота

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@ import os
 
 from dotenv import load_dotenv
 
-from bot import TelegramBot
+from .bot import TelegramBot
 
 
 load_dotenv()
 
 bot = TelegramBot(token=os.getenv("TELEGRAM_BOT_TOKEN")).bot
 
+
+@bot.message_handler(commands=["test"])
+def handle_test_command(message) -> None:
+    """Обрабатывает команду /test и отправляет тестовый ответ пользователю."""
+    bot.send_message(chat_id=message.chat.id, text="Тестовая команда выполнена ✅")
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
 
