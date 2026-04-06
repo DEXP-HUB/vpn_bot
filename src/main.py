c315a61020a9ddede277a9ebf52e335ff3615f61
Ali <ali_guseynov_02@mail.ru>
2026-04-06 16:44:15 +0300

    Создал роутер для wireguard под генерацию файла

--- src/main.py
+++ src/main.py
@@ -2,30 +2,16 @@ import asyncio
 import os
 
 from aiogram import Dispatcher, Bot
-from aiogram.filters import Command
-from aiogram.types import BufferedInputFile, Message
 from dotenv import load_dotenv
 
-from .wireguard.services import WireguardConfiguretor
-from .bot.bot import bot
+from .wireguard.router_configurator import router_configurator
 
 load_dotenv()
 
 bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
 dp = Dispatcher()
 
-
-@dp.message(Command("generate_config"))
-async def generate_config(message: Message) -> None:
-    """Генерирует конфигурацию для клиента WireGuard"""
-    wireguard = WireguardConfiguretor.from_env()
-    wireguard.create_client_keys(client_name="test")
-    config_file = wireguard.create_client_config(client_name="test")
-    wireguard.close()
-    await message.answer_document(
-        document=BufferedInputFile(config_file.read(), filename=config_file.name),
-        caption="Конфигурация успешно сгенерирована ✅",
-    )
+dp.include_router(router_configurator)
 
 
 async def main() -> None:
---
+++ src/wireguard/router_configurator.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import BufferedInputFile, Message
+
+from .services import WireguardConfiguretor
+
+router_configurator = Router()
+
+
+@router_configurator.message(Command("generate_config"))
+async def generate_config(message: Message) -> None:
+    """Генерирует конфигурацию для клиента WireGuard"""
+    wireguard = WireguardConfiguretor.from_env()
+    wireguard.create_client_keys(client_name="test")
+    config_file = wireguard.create_client_config(client_name="test")
+    wireguard.close()
+    await message.answer_document(
+        document=BufferedInputFile(config_file.read(), filename=config_file.name),
+        caption="Конфигурация успешно сгенерирована ✅",
+    )
